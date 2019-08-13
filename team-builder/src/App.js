60e00916a4b47cc20a5d577963d8e3d1c160fe27
MicahJank <MicahJank@users.noreply.github.com>
2019-08-13 16:19:11 -0500

    added state to app.js and created a conditional so that the state will only be rendered when it has information in it

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+
+import styled from 'styled-components';
+
 import './App.css';
 
+const NoMembers = styled.h1`
+  text-align: center;
+`;
+
 function App() {
+
+  const [teamMembers, setTeamMemebers] = useState();
+
+
+  // I am putting in this conditional here because i know i will need to programatically create each new team member
+  // in a seperate component. That component will need to take the array that will be assigned to teamMembers and
+  // map over it. So if i dont have this conditional then that .map wont work at the start because the array will
+  // return truthy and will cause an error on the .map function since there will be no items in it at the start.
+  //In other words, i need this so that i can avoid running the .map until my form has submitted actual data to the
+  // state
+  if (!teamMembers) {
+    return (
+      <NoMembers>You have no Team Members yet!</NoMembers>
+    );
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Please Enter in a Team Member</h1>
     </div>
   );
 }

2f7ecc65788a31c3942be7a4e6a600793a6ccba8
MicahJank <MicahJank@users.noreply.github.com>
2019-08-13 19:18:31 -0500

    created styled components for the form elements

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -11,7 +11,8 @@ import Members from './components/Members.js';
 
 function App() {
 
-  // default value of false is given because i dont want
+  // default value of false is given because i dont want the list in the Members.js to return until there is actual
+  // data inside of teamMembers
   const [teamMembers, setTeamMemebers] = useState(false);
 
   return (
--- team-builder/src/components/Form.js
+++ team-builder/src/components/Form.js
@@ -2,12 +2,55 @@ import React, { useState } from 'react';
 
 import styled from 'styled-components';
 
+const MemberForm = styled.form`
+
+    display: flex;
+    flex-direction: column;
+    width: 30%;
+    margin: auto;
+
+    input, select {
+        height: 30px;
+        padding: 5px;
+    }
+
+`;
+
+const Name = styled.div`
+    margin-bottom: 20px;
+`;
+
+const Email = styled.div`
+    margin-bottom: 20px;
+`;
+
+const Role = styled.div`
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 20px;
+`;
+
 
 const Form = () => {
 
   return (
 
-    <div>This will be a form</div>
+    <MemberForm>
+        <Name>
+            <label htmlFor='name' hidden>Member Name</label>
+            <input name='name' type='text' placeholder='member name' />
+        </Name>
+
+        <Email>
+            <label htmlFor='email' hidden>Email</label>
+            <input name='email' type='email' placeholder='email address' />
+        </Email>
+
+        <Role>
+            <label htmlFor='role' myAttr >Member Role</label>
+            <select name='role' type='' />
+        </Role>
+    </MemberForm>
   );
 
 };

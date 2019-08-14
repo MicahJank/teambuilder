2c97afb139319512c209036329258f263338737e
MicahJank <MicahJank@users.noreply.github.com>
2019-08-14 17:52:37 -0500

    began working on stretch - added a editMember function and passed it down into an onClik on an edit button in the Member component

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -15,6 +15,9 @@ function App() {
   // data inside of teamMembers
   const [teamMembers, setTeamMembers] = useState([]);
 
+  // this hook will be used to edit the individual information of each member
+  const [memberToEdit, setMemberToEdit] = useState({})
+
   // this function will be passed down to the form component because the form is where we want to actually
   // create the new member. Here we can see that we are setting the state of teamMembers and
   // a new member is being added to the teamMembers array. Since the default 
@@ -23,11 +26,20 @@ function App() {
     
   };
 
+  // editMember is a function that will allow the user to edit the member info. It needs to be passed down into
+  // Member.js as a prop so that it can be updated according to the current member that has the 'edit' button clicked
+  const editMember = member => {
+    setMemberToEdit(member);
+    console.log(memberToEdit);
+  };
+
+  
+
   return (
     <div className="App">
       <h1>Please Enter in a Team Member</h1>
       <Form addNewMember={addNewMember} />
-      <Members memberList={teamMembers} />
+      <Members memberList={teamMembers} editFunction={editMember} />
     </div>
   );
 }
--- team-builder/src/components/Members.js
+++ team-builder/src/components/Members.js
@@ -10,10 +10,22 @@ const NoMembers = styled.h3`
 
 const MemberCard = styled.div`
   background-color: white;
-  border: 1px solid black;
+  box-shadow: 0px 8px 21px -4px rgba(125,125,125,1);
+  width: 350px;
+  margin: 50px auto;
+  padding: 30px;
+
+  button {
+    padding: 3px 10px;
+    background-color: #5CBCF6;
+    border: none;
+    border-radius: 2px;
+    color: white;
+    cursor: pointer;
+  }
 `;
 
-const Members = ({ memberList }) => {
+const Members = ({ memberList, editFunction }) => {
    
     // memberList is the array of team members. Since it starts off as an empty array i need to check that there has been data passed through it before i start trying to display that
     // data. So here i am checking if the memberList array contains any values, if it doesnt then that means a form hasnt been submitted yet so it shouldnt return any information. If
@@ -33,6 +45,7 @@ const Members = ({ memberList }) => {
             <h2>{member.name}</h2>
             <h3>{member.email}</h3>
             <h3>{member.role}</h3>
+            <button onClick={() => { editFunction(member) }}>Edit</button>
           </MemberCard>
         );
       })
